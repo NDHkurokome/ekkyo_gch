080a25f28b3d05a62ffeebc1e220c4b8b4dae201
ndh-onok <141895980+ndh-onok@users.noreply.github.com>
2024-12-05 00:57:10 +0000

    feat_ホーム画面の作成 app.jsとapp.cssを作成。 コンテナjsonを修正

--- react-ekkyo-app/src/App.js
+++ react-ekkyo-app/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  Button,
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+} from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="root">
+      <Typography variant="h4" className="header">
+        スキルマッチングサービス
+      </Typography>
+      <Button variant="contained" color="primary" className="button">
+        スキル募集フォーム
+      </Button>
+      <TextField
+        label="検索"
+        variant="outlined"
+        fullWidth
+        className="searchField"
+      />
+      <TableContainer component={Paper}>
+        <Table className="table">
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>募集概要</TableCell>
+              <TableCell>求めるスキル</TableCell>
+              <TableCell>対応可否</TableCell>
+              <TableCell>登録者コメント</TableCell>
+              <TableCell>ゲスト参加</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* ここにデータを追加 */}
+            <TableRow>
+              <TableCell>1</TableCell>
+              <TableCell>例: フロントエンド開発</TableCell>
+              <TableCell>React, JavaScript</TableCell>
+              <TableCell>可</TableCell>
+              <TableCell>経験あり</TableCell>
+              <TableCell>可能</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
 
